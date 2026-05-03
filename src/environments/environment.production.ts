89e99f3d14241d766a7335870f7869db09953f4c
Alberto Linares Muñoz <alinares94@Mac-de-Alberto-y-Jenni.local>
2026-05-03 18:24:21 +0200

    environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+    url: import.meta.env.VITE_SUPABASE_URL,
+    anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
   },
   currency: 'EUR',
 };
