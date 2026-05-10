e5ce8ab5b5b8a290df8c8b9fbc5f5db1701290be
Alberto Linares Muñoz <alinares94@Mac-de-Alberto-y-Jenni.local>
2026-05-10 21:30:54 +0200

    version

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '1.0.0',
+  appVersion: '1.0.1',
   supabase: {
     url: 'https://idagedwvtdogholoyawt.supabase.co',
     anonKey: 'sb_publishable_Bv_8iCxLztD6TvPcvhD-FQ_n2jitWwM',
